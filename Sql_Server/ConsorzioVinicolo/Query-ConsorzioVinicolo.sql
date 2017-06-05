1c1c91cb511349633230b4d6f9ff5645ab6fd95a
Tony8990 <tony.fava@edu.itspiemonte.it>
2017-06-05 15:55:23 +0200

    Creato e aggiunto dataBase consorzio Vinicolo

---
+++ Sql_Server/ConsorzioVinicolo/Query-ConsorzioVinicolo.sql
@@ -0,0 +1,70 @@
+use [ConsorzioVinicolo]
+go
+
+--Query A Modifica Dati di una certa Tabella--
+
+Update Azienda Set Titolare='Pippo Peppe'
+where Id=1000;
+go
+
+--Modifica Di una Certa Tipologia Di Vino--
+
+Update Produzione Set TipologiaVino='Chardoney' where id=1;
+go 
+
+--Eliminare i Dati Di una Certa Tabella--
+
+Delete from Azienda where Titolare='Pippo peppe';
+go
+
+/*
+Query Visualizzare numero di Bottiglie prodotte da 
+Da ciascuna Azienda tra due date fornite
+*/
+
+Select a.nome,SUM(p.BottiglieVendute) as 'Numero bottiglie Prodotte' from 
+Produzione p inner join Azienda a on
+p.IdAzienda=a.Id where p.Data between '2017-05-01' and '2017-12-01'
+group by a.nome
+go
+
+/*
+Visualizzare Media Di uva Lavorata Giornamente Dalle Aziende 
+nell'anno corrente divise per regione
+*/
+
+Select re.Denominazione,AVG(p.UvaLavorata)as 'Quantita Uva Lavorata'
+from Produzione p inner join Azienda az
+on p.IdAzienda=az.Id inner join Comune c
+on az.IdComune=c.Id inner join Provincia pr
+on c.IdProvincia=pr.Id inner join Regione re
+on re.Id=pr.IdRegione 
+where YEAR(Data)=2016
+group by re.Denominazione;
+go
+
+/*
+Visualizzare i Dati dell'azienda che ha venduto 
+il maggior numero di bottiglie di cabernet 
+di origine controllata di una data annata
+*/
+
+Select a.nome,COUNT(*)as 'Bottiglie'
+from Azienda a inner join Cassa c on
+c.IdAzienda=a.Id
+where c.Tipologia='Cabernet'and c.Annata=2011 and c.Denominazione like
+'%DOC%'
+group by a.Nome
+
+/*
+Visualizza l'elenco delle aziende vinicole che in un certo periodo 
+hanno venduto meno di 1000 bottiglie di vino da tavola
+*/
+Select a.Nome,COUNT(*) as 'Bottiglie'
+from Azienda a inner join Cassa c
+on c.IdAzienda=a.Id
+where c.DataVendita between '2016-06-01' and '2016-10-01' and c.Denominazione like '%VDT%'
+group by a.Nome having COUNT(*)<1000
+
+
+go
